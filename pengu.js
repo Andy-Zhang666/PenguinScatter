1078f4609f301392589866003da8b2842bc5ba00
Andy Zhang <andy.zhang@centre.edu>
2020-11-10 20:30:03 -0500

    <update>

--- pengu.js
+++ pengu.js
@@ -1,5 +1,22 @@
+var getGrade=function(pengu)
+{return pengu.grade}
+
+var getFinal = function(student)
+{return student.final[0].grade}
+
+var getMeanHW = function(student)
+{
+    return d3.mean(student.homework.map(getGrade));}
+
+var meanQuizes = function(student)
+{return d3.mean(student.quizes.map(getGrade))}
+
+var getPicture = function(student){
+    return "imgs/"+student.picture;}
+
 var drawPlot = function(kids,screen,xScale,yScale)
 {
+    d3.selectAll("circle").remove()
     d3.select("#graph")
     .selectAll("circle")
     .data(kids)
@@ -13,6 +30,27 @@ var drawPlot = function(kids,screen,xScale,yScale)
           return yScale(getMeanHW(aKid))
     })
     .attr("r",2)
+    .on("mouseenter",function(aKid)
+        {
+        d3.select("img")
+        .remove()
+        
+        var xPos = d3.event.pageX;
+         var yPos = d3.event.pageY;
+         
+       d3.select("#tooltip")
+         .classed("hidden",false)
+         .style("top",yPos+"px")
+         .style("left",xPos+"px")
+         
+        d3.select("#img")
+         .append("img")
+         .attr("src","imgs/"+aKid.picture);   
+         
+//         aKid.final[0].grade
+         
+//         d3.mean(aKid.homework.map(getGrade))
+    })
 }
 
 var initGraph = function(kids)
@@ -35,26 +73,16 @@ var initGraph = function(kids)
 }
 
 
-var getGrade=function(pengu)
-{return pengu.grade}
-
-var getFinal = function(student)
-{return student.final[0].grade}
-
-var getMeanHW = function(student)
-{
-    return d3.mean(student.homework.map(getGrade));} 
-
 var classData = d3.json("classData.json")
 
-var successFCN = function(classData)
-{
-    console.log("Data collected",classData);
-}
-
-
-var failFCN = function(errorMsg)
-{console.log("Whoops, something went wrong",errorMSG);}
+//var successFCN = function(classData)
+//{
+//    console.log("Data collected",classData);
+//}
+//
+//
+//var failFCN = function(errorMsg)
+//{console.log("Whoops, something went wrong",errorMSG);}
 
 //classData.then(successFCN,failFCN);
 
